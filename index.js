a4662ed4fa3d44b3db7d9a5469e90fa46b6399f7
Niti Nrw <niti.patel@flatironschool.com>
2016-11-05 23:28:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+function bumpCounter() {
+  var counter = 0
+
+  function addBump() {
+    counter += 1
+  }
+  // debugger;
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+   addBump,
+   getBumps
+ };
+
+}
+
+function createAnimal(animalType) {
+
+  return function device(deadlyDevice) {
+    return {animalType: `${animalType}`, deadlyDevice: `${deadlyDevice}`}
+  }
+}
+
+  var sharkCreator = createAnimal('Shark')
+  var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+  var sharkWithFrickinCannon = sharkCreator('Cannon')
